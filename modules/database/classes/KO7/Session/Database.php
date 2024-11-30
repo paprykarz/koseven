8a06a754afdf640899431a85c5ad9632ed867f6f
p.s <paprykarz@users.noreply.github.com>
2024-11-30 16:40:55 +0100

    Fix uniqid usage

--- modules/database/classes/KO7/Session/Database.php
+++ modules/database/classes/KO7/Session/Database.php
@@ -126,7 +126,7 @@ class KO7_Session_Database extends Session {
 		do
 		{
 			// Create a new session id
-			$id = str_replace('.', '-', uniqid(NULL, TRUE));
+			$id = str_replace('.', '-', uniqid(more_entropy: true));
 
 			// Get the the id from the database
 			$result = $query->execute($this->_db);
